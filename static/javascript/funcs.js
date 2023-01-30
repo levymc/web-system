2528f8d8dafd81de508b5c2a4da0b3776b5cd2b5
levymc <levymcruz@gmail.com>
2023-01-30 07:18:49 -0300

    config inicial 30/01

--- static/javascript/funcs.js
+++ static/javascript/funcs.js
@@ -23,29 +23,13 @@ $(document).ready(function(){
      });
   });
   
-  $(document).ready(function(){
+$(document).ready(function(){
     $(".sub-btn").click(function(){
         // Abre o dropdown
       $(this).next('.sub-menu2').slideToggle();
         // Rotaciona a 'seta' do dropdown 
       $(this).find('.dropdown').toggleClass('rotate2'); 
     });
-    // Expandindo a sidebar
-    // $('.menu-btn').click(function(){
-    //   $('.side-bar').addClass('active');
-    //   $('.menu-btn').css("visibility", "hidden");
-    //   $('.container').css("filter", "blur(8px) opacity(25%)");
-    //   $('header').css("filter", "blur(8px) opacity(25%)");
-    //   $('.loginFormContainer').css("filter", "blur(8px) opacity(25%)");
-    // })
-    // // Escondendo ela
-    // $('.close-btn ,.container, header').click(function(){
-    //   $('.side-bar').removeClass('active');
-    //   $('.menu-btn').css("visibility", "visible");
-    //   $('.container').css("filter", "none");
-    //   $('header').css("filter", "none");
-    //   $('.loginFormContainer').css("filter", "none");
-    //  });
   });
 
   
