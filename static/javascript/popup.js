9c904642c6d28abb43d1c71892673f8ecb4e803e
levymc <levymcruz@gmail.com>
2023-01-31 06:38:43 -0300

    bugs 31/01

--- static/javascript/popup.js
+++ static/javascript/popup.js
@@ -333,6 +333,7 @@ function solicitacaoCompra(){
       Swal.getPopup().querySelector("#areaUso").value = "";
       console.log(nomeItem);
       if (itens.length==1){
+        document.getElementById("itens").style.background = "rgba(192, 192, 192, 0.75)";
         htmlNovo = `
         <div class="row">
           <div class="col h4 itensNovos" id="titleItens">Itens Adicionados:</div>    
