83e71afb1188336e7d5795c956a6ad44ce843007
levymc <levymcruz@gmail.com>
2023-03-28 16:25:14 -0300

    Adicionado a tratativa de erros e Insert e delete

--- DBfuncs.py
+++ DBfuncs.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, Integer, String, create_engine, and_, func, update, exists, select, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.hybrid import hybrid_property
 from datetime import datetime
 
@@ -47,6 +49,51 @@ class Cotacao(Base):
         ultima_linha = session.query(cls).order_by(cls.id_cotacao.desc()).first().as_dict
         return ultima_linha
     
+    @classmethod
+    def insert(cls, id_solicitacao, id_item, item, usuario, fornecedor, contato_fornecedor, unidade, valor_un, frete, inf_extra, validade_cotacao, status_cotacao):
+        try:
+            cotacao = cls(
+                id_solicitacao=id_solicitacao,
+                id_item=id_item,
+                item=item,
+                usuario=usuario,
+                fornecedor=fornecedor,
+                contato_fornecedor=contato_fornecedor,
+                unidade=unidade,
+                valor_un=valor_un,
+                frete=frete,
+                inf_extra=inf_extra,
+                validade_cotacao=validade_cotacao,
+                status_cotacao=status_cotacao
+            )
+            session.add(cotacao)
+            session.commit()
+            return cotacao
+        except IntegrityError as e:
+            session.rollback()
+            print(f"Erro ao inserir a Solicitação {id_solicitacao}: {str(e)}")
+        except Exception as e:
+            session.rollback()
+            print(f"Erro ao inserir item {id_solicitacao}: {str(e)}")
+        finally:
+            session.close()
+    
+    @classmethod
+    def delete(cls, id_cotacao):
+        session = Session()
+        try:
+            cotacao = session.query(cls).filter(cls.id_cotacao == id_cotacao).one()
+            session.delete(cotacao)
+            session.commit()
+        except NoResultFound:
+            print(f"Não existe cotacao com o id_cotacao {id_cotacao}")
+        except Exception as e:
+            session.rollback()
+            print(f"Erro ao deletar cotacao com o id_cotacao {id_cotacao}: {str(e)}")
+        finally:
+            session.close()
+    
+    
     
 class Itens(Base):
     __tablename__ = "itens"
@@ -81,6 +128,37 @@ class Itens(Base):
     def obter_ultima_linha(cls):
         ultima_linha = session.query(cls).order_by(cls.id_item.desc()).first().as_dict
         return ultima_linha
+    
+    @classmethod
+    def insert(cls, id_solicitacao, id_item, nomeItem, descricao, categoria, classificacao, quantidade, unidade):
+        session = Session()
+        try:
+            item = cls(id_solicitacao=id_solicitacao, id_item=id_item, nomeItem=nomeItem, descricao=descricao, categoria=categoria, classificacao=classificacao, quantidade=quantidade, unidade=unidade)
+            session.add(item)
+            session.commit()
+        except IntegrityError as e:
+            session.rollback()
+            print(f"Erro ao inserir item {id_item}: {str(e)}")
+        except Exception as e:
+            session.rollback()
+            print(f"Erro ao inserir item {id_item}: {str(e)}")
+        finally:
+            session.close()
+    
+    @classmethod
+    def delete(cls, id_solicitacao, id_item):
+        session = Session()
+        try:
+            item = session.query(cls).filter(cls.id_solicitacao == id_solicitacao, cls.id_item == id_item).one()
+            session.delete(item)
+            session.commit()
+        except NoResultFound:
+            print(f"Não existe item com o id_solicitacao {id_solicitacao} e id_item {id_item}")
+        except Exception as e:
+            session.rollback()
+            print(f"Erro ao deletar item com o id_solicitacao {id_solicitacao} e id_item {id_item}: {str(e)}")
+        finally:
+            session.close()
     
     
 class Solicitacao(Base):
